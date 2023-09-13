aa49399b57497ff634e84e1e9b44a2a940caac10
Pedro Costa <pedrocostaalves@live.com.pt>
2023-09-13 10:59:18 +0100

    Repos & Config PUT, POST, DELETE

---
+++ src/main/java/com/capgemini/pedroC/eCommerce/security/DataRestConfig.java
@@ -0,0 +1,26 @@
+package com.capgemini.pedroC.eCommerce.security;
+
+import com.capgemini.pedroC.eCommerce.entity.Product;
+import com.capgemini.pedroC.eCommerce.entity.ProductCategory;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+
+}
