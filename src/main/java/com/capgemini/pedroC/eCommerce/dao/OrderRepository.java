31732ba569a42806827b404eafc2884a10ee8c41
Pedro Costa <pedrocostaalves@live.com.pt>
2023-10-09 11:47:27 +0100

    Order orders by created date

--- src/main/java/com/capgemini/pedroC/eCommerce/dao/OrderRepository.java
+++ src/main/java/com/capgemini/pedroC/eCommerce/dao/OrderRepository.java
@@ -14,4 +14,6 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     Page<Order> findByCustomerEmail(@Param("email") String email, Pageable pageable);
 
+    Page<Order> findByCustomerEmailOrderByDateCreatedDesc(@Param("email") String email, Pageable pageable);
+
 }
